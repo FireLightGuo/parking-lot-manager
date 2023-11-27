f2939c8c208b9f743b1648d1c0a2caad9839f981
GuoWeiLiang <fire-1999@qq.com>
2023-11-27 23:30:18 +0800

    feat: 修改为SpringBoot应用

---
+++ src/main/java/parkinglot/Application.java
@@ -0,0 +1,17 @@
+package parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author GuoWeiLiang
+ * @since 2023/11/27 23:12
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
